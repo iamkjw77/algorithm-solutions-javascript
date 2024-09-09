3787467c3fceae2d3b42c8413053616f7a6ba7da
JIWON KIM <iamkjw77@gmail.com>
2024-09-09 18:30:44 +0900

    feat: 콜라츠 수열 만들기 해결

---
+++ src/programmers/colatzSequence.js
@@ -0,0 +1,10 @@
+function solution(n, arr = []) {
+  arr.push(n);
+
+  if (n === 1) return arr;
+  if (n % 2 === 0) return solution(n / 2, arr);
+
+  return solution(3 * n + 1, arr);
+}
+
+solution(10);
