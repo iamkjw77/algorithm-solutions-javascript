a577e5f91d9569235bfc666eef2e2086bda42037
JIWON KIM <iamkjw77@gmail.com>
2024-10-13 22:20:43 +0900

    feat: 리스트 자르기 해결

---
+++ src/programmers/cutTheList.js
@@ -0,0 +1,11 @@
+const slices = {
+  1: (num_list, [a, b, c]) => num_list.slice(0, b + 1),
+  2: (num_list, [a, b, c]) => num_list.slice(a),
+  3: (num_list, [a, b, c]) => num_list.slice(a, b + 1),
+  4: (num_list, [a, b, c]) =>
+    num_list.slice(a, b + 1).filter((_, i) => i % 2 === 0),
+};
+
+function solution(n, slicer, num_list) {
+  return slice[n](num_list, slicer);
+}
