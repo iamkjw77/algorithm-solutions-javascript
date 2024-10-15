aa4c534467b3275a796c08b0dc4888b8996d710c
JIWON KIM <iamkjw77@naver.com>
2024-10-15 12:08:19 +0900

    feat: 배열만들기3 해결

--- src/programmers/createAnArray03.js
+++ src/programmers/createAnArray03.js
@@ -2,3 +2,11 @@ function solution(arr, intervals) {
   const [[a1, b1], [a2, b2]] = intervals;
   return [...arr.slice(a1, b1 + 1), ...arr.slice(a2, b2 + 1)];
 }
+
+solution(
+  [1, 2, 3, 4, 5],
+  [
+    [1, 3],
+    [0, 4],
+  ]
+);
