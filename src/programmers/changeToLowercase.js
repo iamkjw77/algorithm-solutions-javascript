93b60f12ca5fbb342410701c192987d3d103ddd3
jiwon <iamkjw77@gmail.com>
2024-11-08 14:40:31 +0900

    feat: 소문자로 바꾸기 해결

---
+++ src/programmers/changeToLowercase.js
@@ -0,0 +1,18 @@
+function solution(myString) {
+  let answer = '';
+  const arr = myString.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    let asciiCode = arr[i].charCodeAt();
+
+    if (asciiCode < 97) {
+      answer += String.fromCharCode(asciiCode + 32);
+    } else {
+      answer += arr[i];
+    }
+  }
+
+  return answer;
+}
+
+solution('Apple');
