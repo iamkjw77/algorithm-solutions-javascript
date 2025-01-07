6819eebc2408daaec840e58c31fbfa27ec7fc2f9
jiwon <iamkjw77@gmail.com>
2025-01-07 17:50:18 +0900

    feat: 이차원 배열 대각선 순회하기 해결

---
+++ src/programmers/traversingTwoDimensionalArrayDiagonally.js
@@ -0,0 +1,24 @@
+function solution(board, k) {
+  let sum = 0;
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (i + j <= k) {
+        console.log(i, j);
+        sum += board[i][j];
+      }
+    }
+  }
+
+  return sum;
+}
+
+solution(
+  [
+    [0, 1, 2],
+    [1, 2, 3],
+    [2, 3, 4],
+    [3, 4, 5],
+  ],
+  2
+);
