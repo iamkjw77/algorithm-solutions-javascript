862218d5bbfd1ecf40616721b6307481b1c0b20d
jiwon <iamkjw77@gmail.com>
2025-01-13 23:31:09 +0900

    feat: 문자열 밀기 해결

---
+++ src/programmers/stringPush.js
@@ -0,0 +1,13 @@
+function solution(A, B) {
+  if (A === B) return 0;
+
+  for (let i = 0; i < A.length; i++) {
+    A = A.slice(-1) + A.slice(0, -1);
+    if (A === B) return i + 1;
+  }
+
+  return -1;
+}
+
+solution('hello', 'ohell');
+solution('atat', 'tata');
