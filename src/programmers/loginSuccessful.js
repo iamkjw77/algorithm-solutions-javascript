1c7ae70bfecc44b4e1b1a202df7fca14f027e5cd
jiwon <iamkjw77@gmail.com>
2025-02-12 17:13:34 +0900

    feat: 로그인 성공 해결

---
+++ src/programmers/loginSuccessful.js
@@ -0,0 +1,33 @@
+function solution(id_pw, db) {
+  const [id, pw] = id_pw;
+  const map = new Map(db);
+
+  return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
+}
+
+solution(
+  ['meosseugi', '1234'],
+  [
+    ['rardss', '123'],
+    ['yyoom', '1234'],
+    ['meosseugi', '1234'],
+  ]
+);
+
+solution(
+  ['programmer01', '15789'],
+  [
+    ['programmer02', '111111'],
+    ['programmer00', '134'],
+    ['programmer01', '1145'],
+  ]
+);
+
+solution(
+  ['rabbit04', '98761'],
+  [
+    ['jaja11', '98761'],
+    ['krong0313', '29440'],
+    ['rabbit00', '111333'],
+  ]
+);
