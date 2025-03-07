15255572bf7f21443b4f1b0a9139aa111e803978
jiwon <iamkjw77@gmail.com>
2025-03-07 13:12:44 +0900

    feat: 모음 제거 해결

---
+++ src/programmers/removeVowels.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  return [...my_string].filter((str) => !vowels.includes(str)).join('');
+}
+
+solution('bus');
+solution('nice to meet you');
