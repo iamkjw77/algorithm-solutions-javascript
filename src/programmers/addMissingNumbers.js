1fc2a70fbb5725a992000b66363ad6e7af725523
jiwon <iamkjw77@gmail.com>
2025-05-11 16:09:37 +0900

    feat: 없는 숫자 더하기 해결

---
+++ src/programmers/addMissingNumbers.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+  let sum = 0;
+
+  for (let i = 0; i < 10; i++) {
+    if (!numbers.includes(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+solution([1, 2, 3, 4, 6, 7, 8, 0]);
